cc17d0453e22efcf714b9b2d3353d038bf7e76f3
Matt Joiner <anacrolix@gmail.com>
2017-01-21 16:35:51 +1100

    Just const the directories

--- main.go
+++ main.go
@@ -3,20 +3,26 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 
+	"github.com/anacrolix/missinggo/x"
 	"github.com/anacrolix/tagflag"
+)
+
+const (
+	rootTemplateFile = "base.html"
+	sourceDir        = "source"
+	destDir          = "docs"
+	staticDir        = "static"
 )
 
 func main() {
 	log.SetFlags(log.Flags() | log.Lshortfile)
 	var flags struct {
-		RootTemplateFile string
-		SourceDir        string
-		DestDir          string
 		tagflag.StartPos
 		Command string
 		tagflag.ExcessArgs
@@ -26,13 +32,13 @@ func main() {
 		switch flags.Command {
 		case "build":
 			tagflag.ParseArgs(nil, flags.ExcessArgs)
-			return build(flags.RootTemplateFile, flags.SourceDir, flags.DestDir)
+			return build()
 		case "serve":
 			var serveFlags struct {
 				Addr string
 			}
 			tagflag.ParseArgs(&serveFlags, flags.ExcessArgs)
-			return serve(flags.RootTemplateFile, flags.SourceDir, serveFlags.Addr)
+			return serve(serveFlags.Addr)
 		default:
 			return fmt.Errorf("bad command %q", flags.Command)
 		}
@@ -42,14 +48,42 @@ func main() {
 	}
 }
 
-func build(rootTemplateFile, sourceDir, destDir string) error {
+func copy(srcDir, destDir string) error {
+	return filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		relPath, err := filepath.Rel(srcDir, path)
+		x.Pie(err)
+		dp := filepath.Join(destDir, relPath)
+		os.MkdirAll(filepath.Dir(dp), 0755)
+		log.Printf("copying %q", dp)
+		df, err := os.OpenFile(dp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		x.Pie(err)
+		defer df.Close()
+		sf, err := os.Open(path)
+		x.Pie(err)
+		defer sf.Close()
+		_, err = io.Copy(df, sf)
+		x.Pie(err)
+		return nil
+	})
+}
+
+func build() error {
+	if err := copy(staticDir, destDir); err != nil {
+		return err
+	}
 	log.Printf("parsing %q", rootTemplateFile)
 	t := template.Must(template.ParseFiles(rootTemplateFile))
 	return filepath.Walk(sourceDir, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			return nil
 		}
-		log.Print("parsing %q", path)
+		log.Printf("parsing %q", path)
 		t1 := template.Must(template.Must(t.Clone()).ParseFiles(path))
 		relPath, err := filepath.Rel(sourceDir, path)
 		if err != nil {
@@ -57,7 +91,7 @@ func build(rootTemplateFile, sourceDir, destDir string) error {
 		}
 		destPath := filepath.Join(destDir, relPath)
 		os.MkdirAll(filepath.Dir(destPath), 0755)
-		f, err := os.OpenFile(destPath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
+		f, err := os.OpenFile(destPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 		if err != nil {
 			log.Fatalf("not opening destination file: %s", err)
 		}
@@ -86,11 +120,14 @@ func servePath(root *template.Template, sourceDir, filePath string, w http.Respo
 	}
 	t := template.Must(template.Must(root.Clone()).ParseFiles(filePath))
 	log.Printf("executing template file %q", filePath)
-	t.Execute(w, nil)
+	err = t.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func serveStatic(w http.ResponseWriter, r *http.Request) bool {
-	p := filepath.Join("static", filepath.FromSlash(r.URL.Path))
+	p := filepath.Join(staticDir, filepath.FromSlash(r.URL.Path))
 	info, err := os.Stat(p)
 	if os.IsNotExist(err) {
 		return false
@@ -106,7 +143,7 @@ func serveStatic(w http.ResponseWriter, r *http.Request) bool {
 	return true
 }
 
-func serve(rootTemplateFile, sourceDir, addr string) error {
+func serve(addr string) error {
 	return http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if serveStatic(w, r) {
 			return
